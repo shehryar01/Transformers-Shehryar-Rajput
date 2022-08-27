cb2f69cfc3a971d9785beae99bafdccac763843b
shehryar01 <shehryarajput@hotmail.com>
2022-08-27 13:19:53 +0500

    Initial Commit

---
+++ Transformers Shehryar RajputUITests/Transformers_Shehryar_RajputUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Transformers_Shehryar_RajputUITestsLaunchTests.swift
+//  Transformers Shehryar RajputUITests
+//
+//  Created by macadmin on 27/08/2022.
+//
+
+import XCTest
+
+class Transformers_Shehryar_RajputUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
